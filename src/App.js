6897339d49ab67d4325a7990130e721a4dd4db45
bnunez <bryan.nunez@parivedasolutions.com>
2023-12-28 20:36:13 -0500

    test components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function App() {
   const value = 'Take me to your leader';
-  return <div>Hello World. {value}</div>;
+  return <div><div>MenuBar</div><div>Hello World. {value}</div></div>;
+}
+
+class MenuBar extends React.Component {
+  render() {
+    const menuItems = ["home", "menu", "location", "contact"];
+    const listItems = menuItems.map((item) => {
+      return <li>{item}</li>
+    });
+    return <ul>{listItems}</ul>
+  }
 }
 
 export default App;
