025277ca699ff03b20e66d1ae86e3dddc4559653
bnunez <bryan.nunez@parivedasolutions.com>
2023-12-28 20:39:37 -0500

    test components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
 
-function App() {
-  const value = 'Take me to your leader';
-  return <div><div>MenuBar</div><div>Hello World. {value}</div></div>;
-}
-
 class MenuBar extends React.Component {
   render() {
     const menuItems = ["home", "menu", "location", "contact"];
@@ -13,6 +8,11 @@ class MenuBar extends React.Component {
     });
     return <ul>{listItems}</ul>
   }
+}
+
+function App() {
+  const value = 'Take me to your leader';
+  return <div><div><MenuBar /></div><div>Hello World. {value}</div></div>;
 }
 
 export default App;
